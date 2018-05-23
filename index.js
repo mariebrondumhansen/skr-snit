b9b8272471dd372c7fa13f567e61255b3bc4c91a
Martina <martina959@hotmail.com>
2018-05-23 12:59:08 +0200

    kl.13

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 $(document).ready(function () {
-    $("nav").load("navbar.html");
+    $("nav").load("nav.html");
 })
 
 
 
-//-------------------------------Slideshow
 $("#slideshow > div:gt(0)").hide();
 
 setInterval(function () {
@@ -16,12 +14,4 @@ setInterval(function () {
         .end()
         .appendTo('#slideshow');
 }, 5000);
-=======
-$(document).ready(function(){
-    $("nav").load("nav.html");
-<<<<<<< HEAD:index.js
-});
-=======
-})
->>>>>>> bde67e512df2117a8685c20764e3e2bb0d0ffb4b
->>>>>>> 2cac4f5322ef7da2c8ba05ff9204cd6616de7946:script.js
+// hentet fra https://www.nemprogrammering.dk/wp/simpelt-slideshow-med-css3-og-jquery/
