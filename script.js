eb8ba0952cc38f58ac73f00daee82f7d4a9b5d81
Martina <martina959@hotmail.com>
2018-05-23 09:12:55 +0200

    MMDT

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $("nav").load("navbar.html");
 })
+
+
+
+//-------------------------------Slideshow
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function () {
+    $('#slideshow > div:first')
+        .fadeOut(4000)
+        .next()
+        .fadeIn(2000)
+        .end()
+        .appendTo('#slideshow');
+}, 5000);
