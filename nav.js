63a4ecdf81081031182a28b4f9d4411c914d869e
Martina <martina959@hotmail.com>
2018-05-24 19:23:21 +0200

    Revert "19:18"
    This reverts commit e200e36619cf335c107a03d822a5f58edbf113ac.

--- nav.js
+++ nav.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function () {
     $("#burgerMenu").attr("src", "icons/menu.png")
     $(".dropbtn").click(function () {
@@ -18,25 +17,4 @@ $(document).ready(function () {
             $(this).addClass("uPunkt").removeClass("uPunkt-klik");
         } 
     });
-=======
-$(document).ready(function () {
-    $("#burgerMenu").attr("src", "icons/menu.png")
-    $(".dropbtn").click(function () {
-        if ($("#myDropdown").is(":hidden")) {
-            $("#myDropdown").show();
-            $("#burgerMenu").attr("src", "icons/kryds.png");
-        } else {
-            $("#myDropdown").hide();
-            $("#burgerMenu").attr("src", "icons/menu.png")
-        }
-
-    });
-    $(".uPunkt").click(function() {
-        if ($(this).hasClass("uPunkt")){
-            $(this).addClass("uPunkt-klik").removeClass("uPunkt");
-        } else {
-            $(this).addClass("uPunkt").removeClass("uPunkt-klik");
-        } 
-    });
->>>>>>> MMDT
 });
