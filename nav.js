a3768d82e4276d74f42a474634c811db39b240a1
Martina <martina959@hotmail.com>
2018-05-24 13:38:53 +0200

    opdatering og rigtige navbar dokumenter

---
+++ nav.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $("#burgerMenu").attr("src", "icons/menu.png")
+    $(".dropbtn").click(function () {
+        if ($("#myDropdown").is(":hidden")) {
+            $("#myDropdown").show();
+            $("#burgerMenu").attr("src", "icons/kryds.png");
+        } else {
+            $("#myDropdown").hide();
+            $("#burgerMenu").attr("src", "icons/menu.png")
+        }
+
+    });
+    $(".uPunkt").click(function() {
+        if ($(this).hasClass("uPunkt")){
+            $(this).addClass("uPunkt-klik").removeClass("uPunkt");
+        } else {
+            $(this).addClass("uPunkt").removeClass("uPunkt-klik");
+        } 
+    });
+});
